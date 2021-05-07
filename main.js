7994809c67b8a0675c17edd0bb0cb87b6e11f8b0
Russian hacker boi <77713906+hackerboi72@users.noreply.github.com>
2021-05-07 14:59:52 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+function link1  () {
+    document. getElementById("btn1"). onclick = function () {
+        location.href = "https://malala.org/malalas-story";
+        };
+}
+function link2 () {
+    document. getElementById("btn2"). onclick = function () {
+        location.href = "https://www.youtube.com/watch?v=DcmZpcZl0qg&t=4s";
+        }
+}
+function link3 () {
+    document. getElementById("btn3"). onclick = function () {
+        location.href = "https://www.youtube.com/watch?v=7kYzBld6qYw";
+        }
+}
+function link4 () {
+    document. getElementById("btn4"). onclick = function () {
+        location.href = "https://en.wikipedia.org/wiki/Neil_Armstrong#Life_after_Apollo";
+        }
+}
